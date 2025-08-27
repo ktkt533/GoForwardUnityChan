14490ea08d87286521dcc1ed464e7e5e83e9c277
ktkt533 <ktkt29121@gmail.com>
2025-08-28 06:18:32 +0900

    ユニティちゃんを配置
    チャプター4終了

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem_Actions;
+
+public class UnityChanController : MonoBehaviour
+{
+  //アニメーションするためのコンポーネントを入れる
+  Animator animator;
+  //Unityちゃんを動かすためのコンポーネントを入れる
+  Rigidbody2D rigid2D;
+  //地面の位置
+  private float groundLevel = -3.0f;
+  //ジャンプ速度の減衰
+  private float dump = 0.8f;
+  //ジャンプの速度
+  float jumpVelocity = 20;
+
+  // Start is called before the first frame update 
+  void Start()
+  {
+    //アニメーターのコンポーネントを取得する
+    this.animator = GetComponent<Animator>();
+    //Rigidbody2Dのコンポーネントを取得する
+    this.rigid2D = GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    //走るアニメーションを再生するためにanimatorのパラメータを調整する
+    this.animator.SetFloat("Horizontal", 1);
+    //着地しているかを調べる
+    bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+    this.animator.SetBool("isGround", isGround);
+    //着地状態でクリックされた場合
+    if (isGround && Mouse.current.leftButton.isPressed)
+    {
+      //上方向の力をかける
+      this.rigid2D.linearVelocity = new Vector2(0, this.jumpVelocity);
+    }
+    //クリックをやめたら上方向への速度を減速する
+    if (!Mouse.current.leftButton.isPressed == false)
+    {
+      if (this.rigid2D.linearVelocity.y > 0)
+      {
+        this.rigid2D.linearVelocity *= this.dump;
+      }
+    }
+  }
+}
